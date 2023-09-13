06fc304676cf77115009a07229e81b256261fd26
Oliver L <oliver.lkm@gmail.com>
2023-09-13 11:34:20 +1000

    Add footer element

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import { header } from "./header";
 import { home } from "./home";
 import { menu } from "./menu";
 import { contact } from "./contact";
+import { footer } from "./footer";
 
+const body = document.body;
 const content = document.getElementById("content");
-content.append(header());
+body.insertBefore(header(), content);
 content.append(home());
+body.append(footer());
 
 const clickHandlerNavButtons = function () {
   let buttonName = this.textContent;
-  content.removeChild(content.lastChild);
+  content.removeChild(content.firstChild);
   if (buttonName === "Home") content.append(home());
   if (buttonName === "Menu") content.append(menu());
   if (buttonName === "Contact") content.append(contact());
