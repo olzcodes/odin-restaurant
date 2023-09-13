e0ba746ce485bb99ac4fe08e833744ea34951b88
Oliver L <oliver.lkm@gmail.com>
2023-09-14 00:05:05 +1000

    Add text and image contents

--- src/home.js
+++ src/home.js
@@ -1,6 +1,36 @@
+import Icon from "./images/rune-icon.png";
+
 export function home() {
   const main = document.createElement("main");
-  main.innerHTML = `HOME`;
+
+  const pageContent = {
+    heading: `Where Legends Meet Flavor`,
+    paragraph1: `Step into the realm of Odin's Feast, a fast-food haven inspired by the powerful and enigmatic Norse god, Odin.`,
+    paragraph2: `Here, we blend the ancient tales of Norse mythology with the savory delights of modern fast food to offer you an epic dining experience like no other.`,
+    paragraph3: `Immerse yourself in the mystic ambiance inspired by the halls of Valhalla.`,
+    paragraph4: `Feast upon dishes that pay homage to the bold flavors of the Viking era and contemporary cuisine.`,
+    paragraph5: `Join us on a gastronomic adventure that brings the past to life with every bite.`,
+  };
+
+  for (const [key, value] of Object.entries(pageContent)) {
+    console.log(`${key}: ${value}`);
+    const element = document.createElement("div");
+    element.classList.add("text");
+    element.setAttribute("id", `${key}`);
+    element.textContent = `${value}`;
+    main.append(element);
+  }
+
+  function img() {
+    const img = document.createElement("img");
+    img.src = Icon;
+    img.classList.add("icon");
+    img.setAttribute("title", "Image credits: Flaticon - Rune - 336182");
+
+    return img;
+  }
+
+  main.append(img());
 
   return main;
 }
