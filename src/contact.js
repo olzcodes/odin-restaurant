608a3c3cce87a4a20706f929bb13d058de85fd61
Oliver L <oliver.lkm@gmail.com>
2023-09-14 12:44:12 +1000

    Add text content

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,44 @@
 export function contact() {
   const main = document.createElement("main");
-  main.innerHTML = `CONTACT`;
+
+  const heading = document.createElement("div");
+  heading.classList.add("text");
+  heading.setAttribute("id", "heading");
+  heading.textContent = "We Are Expecting You";
+  main.append(heading);
+
+  const pageContent = {
+    address: {
+      heading: `Address`,
+      content: `Asgardian Way, Hall of Valhalla
+    Norse Realm, NRV 001`,
+    },
+    hours: {
+      heading: `Hours of Operation`,
+      content: `Monday - Friday: 11:00 AM - 10:00 PM
+      <br> Saturday - Sunday: 12:00 PM - 11:00 PM`,
+    },
+    message: {
+      heading: `Thank you`,
+      content: `Thank you for considering Odin's Feast for your divine dining adventure. We look forward to welcoming you to our halls of legendary flavor. Skål!`,
+    },
+  };
+
+  for (const [key, value] of Object.entries(pageContent)) {
+    console.log(key, value);
+    const contactInfo = document.createElement("div");
+    contactInfo.classList.add("contact-info");
+    contactInfo.classList.add(`${key}`);
+    const heading = document.createElement("div");
+    heading.classList.add("heading");
+    heading.innerHTML = `${value.heading}`;
+    const content = document.createElement("div");
+    content.classList.add("content");
+    content.innerHTML = `${value.content}`;
+    contactInfo.append(heading);
+    contactInfo.append(content);
+    main.append(contactInfo);
+  }
 
   return main;
 }
